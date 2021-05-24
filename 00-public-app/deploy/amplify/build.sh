8f071bf4f3552b06f7c450b789ed8a6d71585730
jasonrundell <jason.rundell@gmail.com>
2021-05-24 13:40:36 -0400

    build update

--- 00-public-app/deploy/amplify/build.sh
+++ 00-public-app/deploy/amplify/build.sh
@@ -3,12 +3,12 @@ echo "Deploying branch: ${BRANCH}"
 # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 # export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-# echo "nvm install 16.2.0..."
-# nvm install 16.2.0
-# echo "nvm use 16.2.0..."
-# nvm use 16.2.0
-# echo "node version:"
-# node -v
+echo "nvm install 14.17.0..."
+nvm install 14.17.0
+echo "nvm use 14.17.0..."
+nvm use 14.17.0
+echo "node version:"
+node -v
 # echo "yarn set version 1.22.0"
 # yarn set version 1.22.0
 # echo "yarn version:"
